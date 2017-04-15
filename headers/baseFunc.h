46013310ab1458009836317bb13222ae3bbff847
kartollika <maksimow.dmitrij@yandex.ru>
2017-04-15 13:46:08 +0300

    Create module baseFunc

---
+++ headers/baseFunc.h
@@ -0,0 +1,17 @@
+#ifndef BASEFUNC_H_INCLUDED
+#define BASEFUNC_H_INCLUDED
+#include <string.h>
+#include <assert.h>
+
+typedef struct {
+    char *name,
+         *surname;
+    int place;
+    double average;
+} participant;
+
+
+
+
+#endif // BASEFUNC_H_INCLUDED
+
---
+++ src/baseFucn.c
@@ -0,0 +1,2 @@
+#include "../headers/baseFunc.h"
+
