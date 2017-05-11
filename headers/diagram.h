51306ca7ca73e153099c859ce20e951f198e2bd9
kartollika <maksimow.dmitrij@yandex.ru>
2017-05-11 15:34:54 +0300

    s

---
+++ headers/diagram.h
@@ -0,0 +1,14 @@
+#ifndef INTERFACE_H_INCLUDED
+#define INTERFACE_H_INCLUDED
+#include "baseFunc.h"
+#include <graphics.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void showDiagramRadial(const participant *part, int mode, int column, int record);
+
+void showDiagramColumn(const participant *part, int mode, int column, int record);
+
+#endif // INTERFACE_H_INCLUDED
